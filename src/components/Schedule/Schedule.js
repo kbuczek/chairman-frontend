dd03ae5c7a136edb60d12f1aec61f7f15315cdb0
kbuczek <krzysztofbuczek@live.com>
2021-09-09 18:03:51 +0200

    09.09

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -153,7 +153,6 @@ const Schedule = ({ option, products }) => {
           <Clock />
         </Form>
 
-        {/* {updateDays()} */}
         {days.map((thisDay, index) => {
           if (thisDay === optionDays || optionDays === "all") {
             return (
